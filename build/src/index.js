a3e8df24c2cac37f87895ff8f78ab9fdb31cf7f0
syakir <syakir.uzair@setel.com>
2024-08-11 22:21:55 +0800

    chore: Handle fastest route

--- build/src/index.js
+++ build/src/index.js
@@ -7,61 +7,24 @@ const testCases = [
         title: 'Basic case',
         input: {
             edges: [
-                {
-                    from: 'A',
-                    to: 'B',
-                    distance: 30,
-                },
-                {
-                    from: 'B',
-                    to: 'C',
-                    distance: 10,
-                },
+                { from: 'A', to: 'B', distance: 30 },
+                { from: 'B', to: 'C', distance: 10 },
             ],
             packages: [{ name: 'K1', weight: 5, from: 'A', to: 'C' }],
             trains: [{ name: 'Q1', capacity: 6, start: 'B' }],
         },
         expectedOutput: [
-            {
-                W: 0,
-                T: 'Q1',
-                N1: 'B',
-                P1: [],
-                N2: 'A',
-                P2: [],
-            },
-            {
-                W: 30,
-                T: 'Q1',
-                N1: 'A',
-                P1: ['K1'],
-                N2: 'B',
-                P2: [],
-            },
-            {
-                W: 60,
-                T: 'Q1',
-                N1: 'B',
-                P1: [],
-                N2: 'C',
-                P2: ['K1'],
-            },
+            { W: 0, T: 'Q1', N1: 'B', P1: [], N2: 'A', P2: [] },
+            { W: 30, T: 'Q1', N1: 'A', P1: ['K1'], N2: 'B', P2: [] },
+            { W: 60, T: 'Q1', N1: 'B', P1: [], N2: 'C', P2: ['K1'] },
         ],
     },
     {
         title: 'Invalid package starting location',
         input: {
             edges: [
-                {
-                    from: 'A',
-                    to: 'B',
-                    distance: 30,
-                },
-                {
-                    from: 'B',
-                    to: 'C',
-                    distance: 10,
-                },
+                { from: 'A', to: 'B', distance: 30 },
+                { from: 'B', to: 'C', distance: 10 },
             ],
             packages: [{ name: 'K1', weight: 5, from: 'X', to: 'C' }],
             trains: [{ name: 'Q1', capacity: 6, start: 'B' }],
@@ -72,29 +35,46 @@ const testCases = [
         title: 'Invalid package destination',
         input: {
             edges: [
-                {
-                    from: 'A',
-                    to: 'B',
-                    distance: 30,
-                },
-                {
-                    from: 'B',
-                    to: 'C',
-                    distance: 10,
-                },
+                { from: 'A', to: 'B', distance: 30 },
+                { from: 'B', to: 'C', distance: 10 },
             ],
             packages: [{ name: 'K1', weight: 5, from: 'A', to: 'X' }],
             trains: [{ name: 'Q1', capacity: 6, start: 'B' }],
         },
+        // train is still able to pick up the package
+        expectedOutput: [{ W: 0, T: 'Q1', N1: 'B', P1: [], N2: 'A', P2: [] }],
+    },
+    {
+        title: 'Insufficient train capacity',
+        input: {
+            edges: [
+                { from: 'A', to: 'B', distance: 30 },
+                { from: 'B', to: 'C', distance: 10 },
+            ],
+            packages: [{ name: 'K1', weight: 5, from: 'A', to: 'C' }],
+            trains: [{ name: 'Q1', capacity: 1, start: 'B' }],
+        },
+        expectedOutput: [],
+    },
+    {
+        title: 'Should utilize fastest route even with more checkpoints',
+        input: {
+            edges: [
+                { from: 'A', to: 'B', distance: 40 },
+                { from: 'A', to: 'C', distance: 10 },
+                { from: 'B', to: 'C', distance: 20 },
+                { from: 'B', to: 'D', distance: 10 },
+                { from: 'C', to: 'D', distance: 50 },
+            ],
+            packages: [{ name: 'K1', weight: 5, from: 'A', to: 'D' }],
+            trains: [{ name: 'Q1', capacity: 5, start: 'B' }],
+        },
         expectedOutput: [
-            {
-                W: 0,
-                T: 'Q1',
-                N1: 'B',
-                P1: [],
-                N2: 'A',
-                P2: [],
-            },
+            { W: 0, T: 'Q1', N1: 'B', P1: [], N2: 'C', P2: [] },
+            { W: 20, T: 'Q1', N1: 'C', P1: [], N2: 'A', P2: [] },
+            { W: 30, T: 'Q1', N1: 'A', P1: ['K1'], N2: 'C', P2: [] },
+            { W: 40, T: 'Q1', N1: 'C', P1: [], N2: 'B', P2: [] },
+            { W: 60, T: 'Q1', N1: 'B', P1: [], N2: 'D', P2: ['K1'] },
         ],
     },
 ];
