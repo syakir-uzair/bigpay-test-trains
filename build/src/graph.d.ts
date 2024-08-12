4956426e85d5749be36ec4402c4bc936a79e5b55
syakir <syakir.uzair@setel.com>
2024-08-12 19:14:40 +0800

    chore: Add gitignore

--- build/src/graph.d.ts
+++
@@ -1,8 +0,0 @@
-import { Destination, Route } from './types';
-export declare class Graph {
-    adjList: Map<string, Route[]>;
-    cache: Map<string, Map<string, Destination>>;
-    constructor();
-    addEdge(from: string, to: string, distance: number): void;
-    dijkstra(start: string): Map<string, Destination>;
-}
