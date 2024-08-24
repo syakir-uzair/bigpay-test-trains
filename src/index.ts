5432bda98aba1210813724a5a94a48d554e2f3e4
syakir-uzair <syakir.uzair@setel.com>
2024-08-25 01:55:28 +0800

    feat: Dynamic programming (#3)
    * chore: Add test case
    
    * chore: Basic dynamic programming
    
    * fix: Calculation
    
    * fix: All test cases
    
    * chore: Add cache
    
    * chore: Refactor
    
    * chore: Improve logic
    
    * chore: Clean up
    
    * chore: Update readme
    
    * chore: Improve logic
    
    * chore: Clean up

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ const testCases: TestCase[] = [
     expectedOutput: [],
   },
   {
-    title: 'Should deliver multiple packages if capacity is sufficient',
+    title: 'Should deliver multiple packages if the capacity is sufficient',
     input: {
       edges: [
         {from: 'A', to: 'B', distance: 30},
@@ -70,7 +70,6 @@ const testCases: TestCase[] = [
       ],
       trains: [
         {name: 'Q1', capacity: 5, start: 'B'},
-        // Q2 should be picked for the job
         {name: 'Q2', capacity: 15, start: 'B'},
         {name: 'Q3', capacity: 5, start: 'B'},
       ],
@@ -82,7 +81,7 @@ const testCases: TestCase[] = [
     ],
   },
   {
-    title: 'Should utilize fastest route even with more checkpoints',
+    title: 'Should utilize shortest route, even with more checkpoints',
     input: {
       edges: [
         // its faster to go from to B via C, and faster to go to D via both B and C
@@ -128,10 +127,10 @@ const testCases: TestCase[] = [
     },
     expectedOutput: [
       {W: 0, T: 'Q1', N1: 'A', P1: [], N2: 'X', P2: []},
-      {W: 0, T: 'Q2', N1: 'B', P1: [], N2: 'X', P2: []},
-      {W: 0, T: 'Q3', N1: 'C', P1: [], N2: 'X', P2: []},
       {W: 10, T: 'Q1', N1: 'X', P1: ['K1'], N2: 'D', P2: ['K1']},
+      {W: 0, T: 'Q2', N1: 'B', P1: [], N2: 'X', P2: []},
       {W: 10, T: 'Q2', N1: 'X', P1: ['K2'], N2: 'E', P2: ['K2']},
+      {W: 0, T: 'Q3', N1: 'C', P1: [], N2: 'X', P2: []},
       {W: 10, T: 'Q3', N1: 'X', P1: ['K3'], N2: 'F', P2: ['K3']},
     ],
   },
@@ -161,18 +160,69 @@ const testCases: TestCase[] = [
       {W: 30, T: 'Q2', N1: 'X', P1: ['K2'], N2: 'D', P2: ['K2']},
     ],
   },
+  {
+    title:
+      'Should pick up multiple packages in multiple trains in parallel using shortest route',
+    input: {
+      edges: [
+        {from: 'A', to: 'B', distance: 30},
+        {from: 'B', to: 'G', distance: 30},
+        {from: 'G', to: 'H', distance: 20},
+        {from: 'H', to: 'B', distance: 20},
+        {from: 'B', to: 'C', distance: 100},
+        {from: 'C', to: 'D', distance: 30},
+        {from: 'D', to: 'E', distance: 30},
+        {from: 'C', to: 'F', distance: 50},
+        {from: 'F', to: 'E', distance: 20},
+      ],
+      packages: [
+        {name: 'K1', weight: 5, from: 'A', to: 'G'},
+        {name: 'K2', weight: 5, from: 'A', to: 'H'},
+        {name: 'K3', weight: 5, from: 'B', to: 'H'},
+        {name: 'K4', weight: 5, from: 'H', to: 'E'},
+        {name: 'K5', weight: 5, from: 'E', to: 'A'},
+        {name: 'K6', weight: 5, from: 'F', to: 'C'},
+        {name: 'K7', weight: 5, from: 'F', to: 'G'},
+      ],
+      trains: [
+        {name: 'Q1', capacity: 20, start: 'B'},
+        {name: 'Q2', capacity: 20, start: 'C'},
+      ],
+    },
+    expectedOutput: [
+      {W: 0, T: 'Q1', N1: 'B', P1: [], N2: 'A', P2: []},
+      {W: 30, T: 'Q1', N1: 'A', P1: ['K1', 'K2'], N2: 'B', P2: []},
+      {W: 60, T: 'Q1', N1: 'B', P1: ['K3'], N2: 'H', P2: ['K2', 'K3']},
+      {W: 80, T: 'Q1', N1: 'H', P1: ['K4'], N2: 'G', P2: ['K1']},
+      {W: 100, T: 'Q1', N1: 'G', P1: [], N2: 'B', P2: []},
+      {W: 130, T: 'Q1', N1: 'B', P1: [], N2: 'C', P2: []},
+      {W: 230, T: 'Q1', N1: 'C', P1: [], N2: 'D', P2: []},
+      {W: 290, T: 'Q1', N1: 'D', P1: [], N2: 'E', P2: ['K4']},
+      {W: 0, T: 'Q2', N1: 'C', P1: [], N2: 'D', P2: []},
+      {W: 30, T: 'Q2', N1: 'D', P1: [], N2: 'E', P2: []},
+      {W: 60, T: 'Q2', N1: 'E', P1: ['K5'], N2: 'F', P2: []},
+      {W: 80, T: 'Q2', N1: 'F', P1: ['K6', 'K7'], N2: 'C', P2: ['K6']},
+      {W: 130, T: 'Q2', N1: 'C', P1: [], N2: 'B', P2: []},
+      {W: 230, T: 'Q2', N1: 'B', P1: [], N2: 'A', P2: ['K5']},
+      {W: 260, T: 'Q2', N1: 'A', P1: [], N2: 'B', P2: []},
+      {W: 290, T: 'Q2', N1: 'B', P1: [], N2: 'G', P2: ['K7']},
+    ],
+  },
 ];
 
 function test() {
   let i = 0;
+  // for (const testCase of [testCases[8]]) {
   for (const testCase of testCases) {
     let solution: Output | null = null;
 
     try {
-      console.log(`Running test case ${i} for:`, testCase.title);
+      console.log(`Running test case ${i}: ${testCase.title}`);
+      console.time('Time taken');
       const nav = new Navigation(testCase.input);
       solution = nav.solve();
       assert.deepEqual(solution, testCase.expectedOutput, testCase.title);
+      console.timeEnd('Time taken');
       console.log('Success!');
     } catch (e) {
       console.error('Failed!');
--- src/types.ts
+++ src/types.ts
@@ -17,9 +17,9 @@ export type Train = {
   currentLocation: string;
   capacity: number;
   totalDistance: number;
-  packagesToPickUp: Package[];
-  packagesPickedUp: Package[];
-  packagesDelivered: Package[];
+  packagesToPickUp: string[];
+  packagesPickedUp: string[];
+  packagesDelivered: string[];
 };
 
 export type Package = {
@@ -27,29 +27,25 @@ export type Package = {
   from: string;
   to: string;
   weight: number;
-  pickedUp: boolean;
-  delivered: boolean;
-};
-
-export type TrainPickUpQueue = {
-  train: Train;
-  package: Package;
-  destination: Destination;
-};
-
-export type TrainDeliverQueue = {
-  train: Train;
-  destination: Destination;
+  toBePickedUpBy: string;
+  pickedUpBy: string;
+  deliveredBy: string;
 };
 
 export type Movement = {
   startTime: number;
   endTime: number;
-  train: Train;
   from: string;
   to: string;
-  packagesPickedUp: Package[];
-  packagesDelivered: Package[];
+  train: string;
+  packagesPickedUp: string[];
+  packagesDelivered: string[];
+};
+
+export type Job = {
+  trainName: string;
+  packageName: string;
+  toPickUp: Boolean;
 };
 
 export type Input = {
